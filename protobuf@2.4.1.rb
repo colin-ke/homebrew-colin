166259ceaf3e9548ac8cccb354d5deb756290ad1
colin <keqinwu@163.com>
2019-04-12 15:49:24 +0800

    Update protobuf@2.4.1.rb

--- protobuf@2.4.1.rb
+++ protobuf@2.4.1.rb
@@ -3,12 +3,6 @@ require 'formula'
 class Protobuf < Formula
   url 'https://github.com/protocolbuffers/protobuf/releases/download/v2.4.1/protobuf-2.4.1.tar.bz2'
 
-  fails_with_llvm
-
-  def options
-    [['--universal', 'Do a universal build']]
-  end
-
   def install
     ENV.universal_binary if ARGV.build_universal?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
